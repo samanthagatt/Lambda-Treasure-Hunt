420b9a768a1830c3635b5d2717d92c7192d7bf22
Samantha Gatt <smfgatt@gmail.com>
2019-02-02 16:29:56 -0500

    Adds Xcode project start

---
+++ LambdaMUD Island/LambdaMUD Island/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LambdaMUD Island
+//
+//  Created by Samantha Gatt on 2/2/19.
+//  Copyright © 2019 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
