b062eb72c4f1a2e2fe344268b916e45d42573b9f
Samantha Gatt <smfgatt@gmail.com>
2019-02-07 17:49:45 -0500

    Updates UI

--- LambdaMUD Island/LambdaMUD Island/AppDelegate.swift
+++ LambdaMUD Island/LambdaMUD Island/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        
         APIHelper.shared.getInit { (error, status) in
             if let error = error {
                 print("Error returned: \(error)")
@@ -26,6 +25,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 return
             }
             UserDefaults.standard.set(status.roomID, forKey: TreasureMapHelper.currentRoomIDKey)
+            
+            
+//            let file = "mapFile.json" //this is the file. we will write to and read from it
+//            
+//            if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+//                
+//                let fileURL = dir.appendingPathComponent(file)
+//                
+//                //reading
+//                do {
+//                    let data = try Data(contentsOf: fileURL)
+//                    let obj = try JSONSerialization.jsonObject(with:data, options:[])
+//                    if let dict = obj as? [String: [String: Any]] {
+//                        UserDefaults.standard.set(dict, forKey: TreasureMapHelper.mapKey)
+//                    } else {
+//                        print("Oh no! dict wasn't a [String: String: [Any]]!")
+//                    }
+//                }
+//                catch {
+//                    print("Oh no!")
+//                    return
+//                }
+//            }
         }
         
         return true
--- LambdaMUD Island/LambdaMUD Island/ViewController.swift
+++ LambdaMUD Island/LambdaMUD Island/ViewController.swift
@@ -13,38 +13,37 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        scrollView = UIScrollView()
-//        let (width, height) = setUpMap()
-//        scrollView.contentSize = CGSize(width: width, height: height)
-//        view.addSubview(scrollView)
-//        scrollView.fillSuperview()
-//        scrollView.delegate = self
-        
-//        
-//        do {
-//            let map = TreasureMapHelper.shared.getMap()
-//            let data = try JSONSerialization.data(withJSONObject: map, options: .prettyPrinted)
-//            textView.text = String(data: data, encoding: .utf8)
-//        } catch {
-//            print("error:", error)
-//        }
-        
-        
+        scrollView.backgroundColor = .white
+        let (width, height) = setUpMap()
+        scrollView.contentSize = CGSize(width: width + 20, height: height + 20)
+        view.addSubview(scrollView)
+        setUpCurrentRoom()
+        NotificationCenter.default.addObserver(self, selector: #selector(setUpCurrentRoom), name: UserDefaults.didChangeNotification, object: nil)
     }
     
-    @IBOutlet weak var textView: UITextView!
     
-    // MARK: - UIScrollViewDelegate
-//    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-//        return scrollView
-//    }
-    
-    
+    @objc func setUpCurrentRoom() {
+        DispatchQueue.main.async {
+            self.currentRoomView?.removeFromSuperview()
+            let map = TreasureMapHelper.shared.getMap()
+            let currentRoom = UserDefaults.standard.integer(forKey: TreasureMapHelper.currentRoomIDKey)
+            let coordsString = map[String(currentRoom)]?["coordinates"] as? String ?? "0,0"
+            let subStringArray = coordsString.split(separator: ",")
+            let coords = (x: Int(String(subStringArray[0])) ?? 0,
+                          y: Int(String(subStringArray[1])) ?? 0)
+            let x = (coords.x * self.roomSize) - (self.mapBounds.minX * self.roomSize) + 10
+            let y = (coords.y * self.roomSize) - (self.mapBounds.minY * self.roomSize) + 10
+            self.currentRoomView = UIView(frame: CGRect(x: x, y: y, width: self.squareSize, height: self.squareSize))
+            self.currentRoomView.backgroundColor = UIColor(red:0.56, green:0.93, blue:0.56, alpha:1.0)
+            self.currentRoomView.layer.borderWidth = CGFloat(self.corridorSize)
+            self.currentRoomView.layer.borderColor = UIColor.lightGray.cgColor
+            self.currentRoomView.layer.cornerRadius = CGFloat(self.cornerRadius)
+            self.scrollView.addSubview(self.currentRoomView)
+        }
+    }
     
     
     func setUpMap() -> (Int, Int) {
-        let roomSize = 60
-        var mapBounds: (minX: Int, minY: Int, maxX: Int, maxY: Int) = (Int.max, Int.max, Int.min, Int.min)
         let map = TreasureMapHelper.shared.getMap()
         for (_, valueDict) in map {
             let coordsString = valueDict["coordinates"] as? String ?? "0,0"
@@ -71,37 +70,35 @@ class ViewController: UIViewController, UIScrollViewDelegate {
             let exitsDict = valueDict["exits"] as? [String: Int] ?? [:]
             let exits = exitsDict.keys
             
-            let x = (coords.x * roomSize) - (mapBounds.minX * roomSize)
-            let y = (coords.y * roomSize) - (mapBounds.minY * roomSize)
-            let squareSize = roomSize * 3 / 4
+            let x = (coords.x * roomSize) - (mapBounds.minX * roomSize) + 10
+            let y = (coords.y * roomSize) - (mapBounds.minY * roomSize) + 10
             let leftoverSize = roomSize / 4
             let halfSquare = squareSize / 2
-            let corridorSize = 5
             
             let roomView = UIView(frame: CGRect(x: x, y: y, width: squareSize, height: squareSize))
             roomView.backgroundColor = .clear
             roomView.layer.borderWidth = CGFloat(corridorSize)
             roomView.layer.borderColor = UIColor.lightGray.cgColor
-            roomView.layer.cornerRadius = CGFloat(squareSize / 10 + 2)
+            roomView.layer.cornerRadius = CGFloat(cornerRadius)
             scrollView.addSubview(roomView)
             
             if exits.contains("n") {
-                let corridor = UIView(frame: CGRect(x: x + halfSquare - (corridorSize / 2), y: y + squareSize, width: corridorSize, height: leftoverSize / 2 + 1))
+                let corridor = UIView(frame: CGRect(x: x + halfSquare - (corridorSize / 2), y: y + squareSize, width: corridorSize, height: leftoverSize / 2 + 2))
                 corridor.backgroundColor = .lightGray
                 scrollView.addSubview(corridor)
             }
             if exits.contains("s") {
-                let corridor = UIView(frame: CGRect(x: x + halfSquare - (corridorSize / 2), y: y - (leftoverSize / 2), width: corridorSize, height: leftoverSize / 2 + 1))
+                let corridor = UIView(frame: CGRect(x: x + halfSquare - (corridorSize / 2), y: y - (leftoverSize / 2), width: corridorSize, height: leftoverSize / 2 + 2))
                 corridor.backgroundColor = .lightGray
                 scrollView.addSubview(corridor)
             }
             if exits.contains("e") {
-                let corridor = UIView(frame: CGRect(x: x + squareSize, y: y + halfSquare - (corridorSize / 2), width: leftoverSize / 2 + 1, height: corridorSize))
+                let corridor = UIView(frame: CGRect(x: x + squareSize, y: y + halfSquare - (corridorSize / 2), width: leftoverSize / 2 + 2, height: corridorSize))
                 corridor.backgroundColor = .lightGray
                 scrollView.addSubview(corridor)
             }
             if exits.contains("w") {
-                let corridor = UIView(frame: CGRect(x: x - (leftoverSize / 2), y: y + halfSquare - (corridorSize / 2), width: leftoverSize / 2 + 1, height: corridorSize))
+                let corridor = UIView(frame: CGRect(x: x - (leftoverSize / 2), y: y + halfSquare - (corridorSize / 2), width: leftoverSize / 2 + 2, height: corridorSize))
                 corridor.backgroundColor = .lightGray
                 scrollView.addSubview(corridor)
             }
@@ -111,53 +108,36 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     }
     
     
-    var scrollView: UIScrollView!
-    
-    @IBOutlet var backTo0Button: UIButton!
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet weak var sideBarView: UIView!
     @IBOutlet weak var treasureButton: UIButton!
+    @IBOutlet weak var destinationTextField: UITextField!
+    @IBOutlet var travelButton: UIButton!
     
-    
-    @IBAction func travelBackTo0(_ sender: Any) {
-        
-        let currentRoom = UserDefaults.standard.integer(forKey: TreasureMapHelper.currentRoomIDKey)
-        var p = TreasureMapHelper.getPath(from: currentRoom, to: 1)
-        let nextMove = p.removeFirst()
-        APIHelper.shared.travel(nextMove.dir, nextRoomID: nextMove.room) { (error, status) in
-            if let _ = error, status == nil {
-                p.insert(nextMove, at: 0)
-                print("ERRORRRRRRR")
-            }
-            print("Traveled to room: \(String(status?.roomID ?? 0))")
-            let cd = status?.cooldown ?? 15.0
-            let cooldown = Int(ceil(cd))
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(cooldown)) {
-                self.travel(path: p)
-            }
-        }
+    var currentRoomView: UIView!
+    var roomSize = 60
+    var squareSize: Int {
+        return roomSize * 3 / 4
     }
+    var corridorSize = 6
+    var cornerRadius: Int {
+        return squareSize / 10 + 2
+    }
+    var mapBounds: (minX: Int, minY: Int, maxX: Int, maxY: Int) = (Int.max, Int.max, Int.min, Int.min)
+    
     
     @IBAction func toggleCollectTreasure(_ sender: Any) {
         TreasureMapHelper.shared.getRandomTreasure() { (_, _) in }
     }
     
-    
-    func travel(path: [(dir: String, room: Int)]) {
-        if path.count > 0 {
-            var p = path
-            let nextMove = p.removeFirst()
-            APIHelper.shared.travel(nextMove.dir, nextRoomID: nextMove.room) { (error, status) in
-                if let _ = error, status == nil {
-                    p.insert(nextMove, at: 0)
-                    print("ERRORRRRRRR")
-                }
-                print("Traveled to room: \(String(status?.roomID ?? 0))")
-                let cd = status?.cooldown ?? 15.0
-                let cooldown = Int(ceil(cd))
-                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(cooldown)) {
-                    self.travel(path: p)
-                }
-            }
+    @IBAction func travel(_ sender: Any) {
+        guard let destString = destinationTextField.text,
+            let dest = Int(destString) else {
+            return
         }
+        let path = TreasureMapHelper.getPath(to: dest)
+        TreasureMapHelper.travelTo(path: path)
+        travelButton.isEnabled = false
     }
 }
 
